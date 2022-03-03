55854f0636a99dbe3806bfc3f92e03cc2de4089f
TMoraes2021 <81197363+TMoraes2021@users.noreply.github.com>
2022-03-03 19:27:15 -0300

    Add files via upload

--- PCA-main/Consulta/script.js
+++ PCA-main/Consulta/script.js
@@ -3,7 +3,20 @@ function Acessar(){
     var quantVet = parseInt(campoLogin.length);
 
     var nomeCompletoFuncionario = document.getElementById("nomeCompletoFuncionario");
+    var idadeFuncionario = document.getElementById("idadeFuncionario");
+    var grupoSanguineoFuncionario = document.getElementById("grupoSanguineoFuncionario");
+    var problemasSaudeFuncionario = document.getElementById("problemasSaudeFuncionario");
+    var alergiaFuncionario = document.getElementById("alergiaFuncionario");
+    var pesoFuncionario = document.getElementById("pesoFuncionario");
+    var alturaFuncionario = document.getElementById("alturaFuncionario");
+
     var nomeCompletoPaciente = document.getElementById("nomeCompletoPaciente");
+    var idadePaciente = document.getElementById("idadePaciente");
+    var grupoSanguineoPaciente = document.getElementById("grupoSanguineoPaciente");
+    var problemasSaudePaciente = document.getElementById("problemasSaudePaciente");
+    var alergiaPaciente = document.getElementById("alergiaPaciente");
+    var pesoPaciente = document.getElementById("pesoPaciente");
+    var alturaPaciente = document.getElementById("alturaPaciente");
 
     
     var autenticacao = document.getElementById("autenticacao");
@@ -11,11 +24,23 @@ function Acessar(){
 
     if(campoLogin == "12345"){
         nomeCompletoFuncionario.value = "Alcimar";
+        idadeFuncionario.value = "52";
+        grupoSanguineoFuncionario.value = "O+";
+        problemasSaudeFuncionario.value = "Diabético";
+        alergiaFuncionario.value = "Novalgina";
+        pesoFuncionario.value = "75 kg";
+        alturaFuncionario.value = "1,60 m";
         acessoFuncionario.className = "acessoFuncionarioMostrar";
         autenticacao.className = "autenticacaoOcultar";
     }
     else if(campoLogin == "00123456789"){
         nomeCompletoPaciente.value = "Lucas";
+        idadePaciente.value = "20";
+        grupoSanguineoPaciente.value = "O-";
+        problemasSaudePaciente.value = "hipertenso";
+        alergiaPaciente.value = "Novalgina";
+        pesoPaciente.value = "80 kg";
+        alturaPaciente.value = "1,80 m";
         acessoPaciente.className = "acessoPacienteMostrar";
         autenticacao.className = "autenticacaoOcultar";
     }
